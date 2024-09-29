c0baf08a0ca3c28ae36e40b0c389838521159dca
Reynaldev <ReynDev@proton.me>
2024-09-29 14:43:46 +0700

    Added Folium map in Streamlit app

--- index.py
+++ index.py
@@ -1,34 +1,10 @@
-import folium
-import geopandas
-import numpy as np
-import pandas as pd
-import seaborn as sns
 import datetime as dt
 import streamlit as st
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-
-from folium import plugins
-
-def make_map():
-    geoloc_df = pd.read_csv('/home/archie/Machine-Learning/Datasets/E-Commerce_Public_Dataset/geolocation_dataset.csv')
-    customer_df = pd.read_csv('/home/archie/Machine-Learning/Datasets/E-Commerce_Public_Dataset/customers_dataset.csv')
-    # Buat visualisasi persebaran pembeli menggunakan Folium
-
-    geometry = geopandas.points_from_xy(customer_full_df.geolocation_lng, customer_full_df.geolocation_lat)
-    geo_df = geopandas.GeoDataFrame(customer_full_df, geometry=geometry)
-
-    world = geopandas.read_file(geodatasets.get_path("naturalearth.land"))
-
-    map = folium.Map(location=[15, 30], tiles="Cartodb dark_matter", zoom_start=2)
-
-    plugins.FastMarkerCluster([[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry]).add_to(map)
-
-    map
+import streamlit.components.v1 as components
 
 st.title('Analisis dan Visualisasi Data Menggunakan Matplotlib, Folium, Seaborn, dan GeoPandas')
 
-st.subheader('Peringkat Produk Berdasarkan Rating atau Penjualannya')
+st.header('Peringkat Produk Berdasarkan Rating atau Penjualannya')
 
 tab1, tab2 = st.tabs(['Ratings', 'Sales'])
 
@@ -38,12 +14,24 @@ with tab1:
 with tab2:
     st.image('./assets/prod-total-sales.png')
 
-st.subheader('Grafik RFM')
+st.header('Grafik RFM')
 
 st.image('./assets/rfm.png')
 
-st.subheader('Grafik Histogram')
+st.header('Grafik Histogram')
 
 st.image('./assets/histplot.png')
+
+st.header('Grafik Persebaran Pembeli')
+
+map_html = ""
+with open('./map.html', 'r') as file:
+    for line in file:
+        map_html += line
+
+components.html(map_html, height=500, scrolling=True)
+
+with st.sidebar:
+    st.title('Daftar Isi')
 
 st.caption('Copyright &copy; Reynaldev {}'.format(dt.datetime.now().strftime('%Y')))
