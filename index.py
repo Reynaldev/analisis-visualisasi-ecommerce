e0d285b8310cbb8c978f4b21d20e981def288627
Reynaldev <ReynDev@proton.me>
2024-09-29 12:33:18 +0700

    First commit

--- index.py
+++ index.py
@@ -10,6 +10,22 @@ import matplotlib.ticker as ticker
 
 from folium import plugins
 
+def make_map():
+    geoloc_df = pd.read_csv('/home/archie/Machine-Learning/Datasets/E-Commerce_Public_Dataset/geolocation_dataset.csv')
+    customer_df = pd.read_csv('/home/archie/Machine-Learning/Datasets/E-Commerce_Public_Dataset/customers_dataset.csv')
+    # Buat visualisasi persebaran pembeli menggunakan Folium
+
+    geometry = geopandas.points_from_xy(customer_full_df.geolocation_lng, customer_full_df.geolocation_lat)
+    geo_df = geopandas.GeoDataFrame(customer_full_df, geometry=geometry)
+
+    world = geopandas.read_file(geodatasets.get_path("naturalearth.land"))
+
+    map = folium.Map(location=[15, 30], tiles="Cartodb dark_matter", zoom_start=2)
+
+    plugins.FastMarkerCluster([[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry]).add_to(map)
+
+    map
+
 st.title('Analisis dan Visualisasi Data Menggunakan Matplotlib, Folium, Seaborn, dan GeoPandas')
 
 st.subheader('Peringkat Produk Berdasarkan Rating atau Penjualannya')
